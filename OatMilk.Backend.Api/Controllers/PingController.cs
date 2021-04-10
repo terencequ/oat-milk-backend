c188b99b23915c473569e69f062e14b7399f9f23
Terence <terence@terencequ.com>
2021-04-10 17:41:46 +1200

    Basic ping controller

---
+++ OatMilk.Backend.Api/Controllers/PingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OatMilk.Backend.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        public PingController()
+        {
+
+        }
+
+        [HttpGet("")]
+        public ActionResult<bool> Ping()
+        {
+            return true;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<bool> PingAuth()
+        {
+            return true;
+        }
+    }
+}
