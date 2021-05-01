6710902f3d668b0cfee487e74c211b073a17b248
Terence <terence@terencequ.com>
2021-05-01 18:45:09 +1200

    Pagination, CreateEffect endpoint

--- OatMilk.Backend.Api/Controllers/EffectController.cs
+++ OatMilk.Backend.Api/Controllers/EffectController.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OatMilk.Backend.Api.Services.Abstraction;
+using OatMilk.Backend.Api.Services.Models.Requests;
+using OatMilk.Backend.Api.Services.Models.Responses;
 
 namespace OatMilk.Backend.Api.Controllers
 {
     [Authorize]
     [ApiController]
     [Route("[controller]")]
-    public class EffectController
+    public class EffectController : ControllerBase
     {
+        private readonly IEffectService _effectService;
+
+        public EffectController(IEffectService effectService)
+        {
+            _effectService = effectService;
+        }
         
+        [HttpPost("")]
+        [ProducesResponseType(typeof(AbilityResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<EffectResponse>> CreateEffect([FromBody] EffectRequest request)
+        {
+            try
+            {
+                return await _effectService.CreateEffect(request);
+            }
+            catch (ArgumentException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
     }
 }
