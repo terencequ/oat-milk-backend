7fcedf164368d122b66a8845d2d6b0bd7c10b692
Terence <terence@terencequ.com>
2021-05-09 13:23:40 +1200

    Unit tests for effect service

--- OatMilk.Backend.Api.Tests/Services/EffectServiceTests.cs
+++ OatMilk.Backend.Api.Tests/Services/EffectServiceTests.cs
@@ -57,16 +57,82 @@ namespace OatMilk.Backend.Api.Tests.Services
         
         #region GetEffectByName
 
+        [Test]
+        public async Task GetEffectByName_EffectExists_ShouldReturnAbilityResponse()
+        {
+            const string expectedName = "test";
+
+            var service = new Fixture(new Effect(){ Name = expectedName }).GetSut();
+            var result = await service.GetEffectByName(expectedName);
+            Assert.AreEqual(expectedName, result.Name);
+        }
+        
+        [Test]
+        public void GetEffectByName_EffectDoesntExist_ShouldThrowArgumentException()
+        {
+            const string expectedName = "test";
+
+            var service = new Fixture().GetSut();
+            Assert.ThrowsAsync<ArgumentException>(async () => await service.GetEffectByName(expectedName));
+        }
         
         #endregion
         
         #region UpdateEffect
 
+        [Test]
+        public async Task UpdateEffect_EffectExists_ShouldReturnEffectResponse()
+        {
+            var expectedId = Guid.NewGuid();
+            const string expectedName = "new name";
+            
+            var service = new Fixture(new Effect()
+            {
+                Id = expectedId,
+                Name = "name"
+            }).GetSut();
+            var result = await service.UpdateEffect(expectedId, new EffectRequest()
+            {
+                Name = expectedName
+            });
+            
+            Assert.AreEqual(expectedName, result.Name);
+        }
+        
+        [Test]
+        public void UpdateAbility_EffectDoesntExist_ShouldThrowArgumentException()
+        {
+            const string expectedName = "new name";
+            var service = new Fixture().GetSut();
+
+            Assert.ThrowsAsync<ArgumentException>(async () => await service.UpdateEffect(Guid.NewGuid(), new EffectRequest()
+            {
+                Name = expectedName
+            }));
+        }
         
         #endregion
         
         #region DeleteEffect
 
+        [Test]
+        public void DeleteEffect_EffectExists_Returns()
+        {
+            var expectedGuid = Guid.NewGuid();
+            var service = new Fixture(new Effect()
+            {
+                Id = expectedGuid
+            }).GetSut();
+
+            Assert.DoesNotThrowAsync(async () => await service.DeleteEffect(expectedGuid));
+        }
+        
+        [Test]
+        public void DeleteEffect_EffectDoesntExist_ThrowsArgumentException()
+        {
+            var service = new Fixture().GetSut();
+            Assert.ThrowsAsync<ArgumentException>(async () => await service.DeleteEffect(Guid.NewGuid()));
+        }
         
         #endregion
     }
