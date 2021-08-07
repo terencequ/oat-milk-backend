166f99e38a9e2a97f2c8c42f18d059ca5281312e
Terence <terenco12345@gmail.com>
2021-08-07 23:38:52 +1200

    Typescript Axios SDK

--- OatMilk.Backend.Api/generate-openapi.bat
+++ OatMilk.Backend.Api/generate-openapi.bat
@@ -1,4 +1,5 @@
 dotnet tool restore
 dotnet build
 dotnet swagger tofile --output ./openapi.json ./bin/Debug/net5.0/OatMilk.Backend.Api.dll v1
+npx @openapitools/openapi-generator-cli generate
 pause
