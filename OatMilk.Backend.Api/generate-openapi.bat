0927810c262548a7697f4f2b3207bc21d1dcd3a5
Terence <terenco12345@gmail.com>
2021-08-07 23:16:15 +1200

    change file name, script to generate open api

--- OatMilk.Backend.Api/generate-openapi.bat
+++ OatMilk.Backend.Api/generate-openapi.bat
@@ -1,4 +1,4 @@
 dotnet tool restore
 dotnet build
-dotnet swagger tofile --output ./backend.json ./bin/Debug/net5.0/OatMilk.Backend.Api.dll v1
+dotnet swagger tofile --output ./openapi.json ./bin/Debug/net5.0/OatMilk.Backend.Api.dll v1
 pause
