97e861ba205bbe7428f19ebecfdef1b066bb83c4
Terence <terenco12345@gmail.com>
2021-08-17 16:43:10 +1200

    Configure all models as "required", and enums represented as strings

---
+++ OatMilk.Backend.Api/AspNet/OatMilkSchemaFilter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace OatMilk.Backend.Api.AspNet {
+    
+    /// <summary>
+    /// This schema filter will make every property 'required' for response models,
+    /// for easier processing by the frontend.
+    /// </summary>
+    public class OatMilkSchemaFilter : ISchemaFilter
+    {
+        private readonly HashSet<OpenApiSchema> _valueTypes = new HashSet<OpenApiSchema>();
+
+        public void Apply(OpenApiSchema model, SchemaFilterContext context)
+        {
+            if (model.Properties != null)
+            {
+                foreach (var prop in model.Properties) {
+                    model.Required.Add(prop.Key);
+                }
+            }
+        }
+    }
+}
