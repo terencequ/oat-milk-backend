6d81719238842f8f681649d058365c27bed534da
Terence Qu <therealterencequ@gmail.com>
2021-11-27 17:18:43 +1300

    Don't run script on build

--- OatMilk.Backend.Api/generate-openapi.sh
+++ OatMilk.Backend.Api/generate-openapi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dotnet tool restore
+dotnet build
 dotnet swagger tofile --output ./openapi.json ./bin/Debug/net5.0/OatMilk.Backend.Api.dll v1
 npx @openapitools/openapi-generator-cli generate
 
